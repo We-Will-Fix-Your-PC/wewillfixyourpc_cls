4e7fb195a6d7e6f6588e0f1140dff5aa237a129d
Q <q@misell.cymru>
2020-02-23 20:06:09 +0000

    context processors in prod

--- wewillfixyourpc_cls/settings.py
+++ wewillfixyourpc_cls/settings.py
@@ -93,6 +93,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cls.context_processors.facebook_processor',
+                'cls.context_processors.keycloak_processor',
             ],
         },
     },
