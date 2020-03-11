b3cfa992f23d3da9f9591fc3738308ff390718e7
Q 🦄 <q@misell.cymru>
2020-03-11 11:20:49 +0000

    get customers more efficiently

--- customers/views.py
+++ customers/views.py
@@ -23,15 +23,16 @@ twilio_client = twilio.rest.Client(settings.TWILIO_ACCOUNT, settings.TWILIO_TOKE
 @login_required
 @permission_required('customers.view_customer', raise_exception=True)
 def view_customers(request):
+    client = django_keycloak_auth.clients.get_keycloak_admin_client()
     customers = sorted(
         list(
             map(
-                lambda u: tasks.get_user(u._user_id),
-                filter(
-                    lambda u: next(filter(
-                        lambda r: r.get('name') == 'customer', u.role_mappings.realm.get()
-                    ), False),
-                    django_keycloak_auth.users.get_users()
+                lambda u: tasks.get_user(u.get("id")),
+                client._client.get(
+                    url=client._client.get_full_url(
+                        'auth/admin/realms/{realm}/roles/{role_name}/users'
+                            .format(realm=client._name, role_name="customer")
+                    )
                 )
             )
         ),
