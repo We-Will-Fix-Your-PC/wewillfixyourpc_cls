6320d26bd25e5cfb4d41ee6b2e3b8ce41de61800
Q <q@misell.cymru>
2021-08-21 11:32:52 +0100

    add models to admin

--- scrap/admin.py
+++ scrap/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Item)
+admin.site.register(models.ItemImage)
+admin.site.register(models.ItemCategory)
--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Job)
+admin.site.register(models.Status)
+admin.site.register(models.EquipmentType)
+admin.site.register(models.Location)
+admin.site.register(models.OSType)
+admin.site.register(models.Ticket)
+admin.site.register(models.TicketImage)
+admin.site.register(models.TicketRevision)
